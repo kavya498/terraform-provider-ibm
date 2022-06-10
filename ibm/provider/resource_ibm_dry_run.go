afd4c74537606910947d92f8497efcf4fdc20d63
hkavya26 <hkavya26@in.ibm.com>
2022-06-10 12:51:24 +0530

    fix: dryrun

--- ibm/provider/resource_ibm_dry_run.go
+++ ibm/provider/resource_ibm_dry_run.go
@@ -31,16 +31,10 @@ func resourceIBMDryRunCreate(d *schema.ResourceData, meta interface{}) error {
 
 func resourceIBMDryRunRead(d *schema.ResourceData, meta interface{}) error {
 	d.SetId("testdryrun")
-	log.Println("***********")
 	for k, v := range dryRunSchema {
-		log.Println(k)
-		log.Println(v.Type)
-		// d.Set(k, "dryrun")
 		if v.Computed {
 			value := flattenSchemaElements(v)
-			log.Println("******[1]******", value)
 			d.Set(k, value)
-			log.Println("******[1SET1]******")
 		}
 
 	}
@@ -56,67 +50,57 @@ func resourceIBMDryRunDelete(d *schema.ResourceData, meta interface{}) error {
 }
 
 func flattenSchemaElements(v *schema.Schema) interface{} {
-	log.Println("*******[2]******", v.Type)
 	switch v.Type {
 	case schema.TypeBool:
 		result := true
-		log.Println("*******[3]******", result)
 		return result
 	case schema.TypeString:
-		result := "dryrun"
-		log.Println("*******[4]******", result)
 		return "dryrun"
 	case schema.TypeInt:
 		return 1
 	case schema.TypeSet:
 		result := flattenTypeSet(v)
-		log.Println("*******[5]******", result)
 		return result
 	case schema.TypeList:
+		log.Println("**********[ENTER]*********", v)
 		result := flattenTypeSet(v)
-		log.Println("*******[6]******", result)
 		return result
 	}
 	return "dryrun"
 }
 
 func flattenTypeSet(v *schema.Schema) interface{} {
-	log.Println("[FLATTEN]", v)
 	if v.Elem != nil {
-		log.Println("***********[77]**********")
 		if e, ok := v.Elem.(*schema.Schema); ok {
 			if e.Type == schema.TypeString {
-				log.Println("***********[7]**********")
 				return []string{"dryrun"}
 			}
 			if e.Type == schema.TypeInt {
-				log.Println("***********[8]**********")
 				return []int{0}
 			}
 		}
 
 		if es, ok := v.Elem.(*schema.Resource); ok {
-			log.Printf("***********[111]********** %+v", es.Schema)
-			// var recursiveFlattenList func(*schema.Schema) interface{}
-			// recursiveFlattenList = func(v *schema.Schema) interface{} {
-			log.Println("***********[9]**********")
+			log.Printf("***********[1]************ %+v", es)
+			elemSchemas := make([]map[string]interface{}, 0)
 			elemSchema := make(map[string]interface{})
 			for k, value := range es.Schema {
-				log.Println("***********[99]**********", k, v)
-				if value.Type != schema.TypeList && value.Type != schema.TypeSet && value.Type != schema.TypeMap {
-					flattenedValue := flattenSchemaElements(v)
-					elemSchema[k] = flattenedValue
-					log.Println("***********[90]**********", elemSchema)
-					return elemSchema
-				} else {
-					log.Println("***********[10]**********", elemSchema)
-					return nil
-					// recursiveFlattenList(value)
+				log.Printf("***********[2222]************%s %+v", k, value)
+				flattenedValue := flattenSchemaElements(value)
+				log.Printf("***********[flattenedValue]************%s %+v", k, flattenedValue)
+				if k == "group_id" {
+					log.Println("***********[GROUPPPs]************", flattenedValue)
 				}
+				if k == "rules" {
+					log.Println("***********[ROLESS]************", flattenedValue)
+				}
+				elemSchema[k] = flattenedValue
+				elemSchemas = append(elemSchemas, elemSchema)
+				log.Println("***********[elemROLESS]************", elemSchemas)
+				return elemSchemas
 
 			}
-			return nil
-			// }
+			return elemSchemas
 		}
 	}
 
